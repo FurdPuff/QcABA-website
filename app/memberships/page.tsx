6f09b064f61e1b24beb09acb5b58363d697f8cc3
Nihal M <nihalmir.bc@gmail.com>
2024-09-19 18:59:29 -0400

    membership

--- app/memberships/page.tsx
+++ app/memberships/page.tsx
@@ -1,4 +1,4 @@
-import Navbar from "@/components/Navbar";
+import Navbar from "@/components/Navbar"
 import Footer from "@/components/Footer"
 
 export default function memberships() {
@@ -13,8 +13,88 @@ export default function memberships() {
                     At QcABA, we offer various types of memberships tailored to meet the diverse needs and interests of our members. Whether you're a seasoned behavior analyst, a student aspiring to enter the field, or you are committed to promoting behavior analysis, we have a membership option for you.
                 </h4>
             </main>
-            <div style={{marginBottom: '300px'}}>
+
+            <div>
+                <h1 style={{marginTop: '150px', marginBottom: '50px', textAlign: 'center'}}>
+                    Choose your membership
+                </h1>
             </div>
+
+            <div className="flex justify-center space-x-8"
+                 style={{marginBottom: '100px', textAlign: 'center', position: 'relative', color: 'black'}}>
+                <div style={{width: '20%', backgroundColor: 'white', height: '700px'}}>
+                    <h3 style={{marginTop: '20px', marginBottom: '40px'}}>
+                        Membre étudiant/Student Member
+                    </h3>
+                    <h3 style={{marginLeft: '-110px', color: 'blue'}}>
+                        $
+                    </h3>
+                    <h1 style={{marginTop: '-30px', color: 'blue', fontSize: '80px'}}>
+                        20
+                    </h1>
+                    <h4 style={{padding: '20px'}}>
+                        Ideal for aspiring behavior analysts.
+                    </h4>
+                    <h5>
+                        Valid for 12 months
+                    </h5>
+                </div>
+                
+                <div style={{width: '20%', backgroundColor: 'white', height: '700px'}}>
+                    <h3 style={{marginTop: '20px', marginBottom: '40px'}}>
+                        Membre Affilié/Affiliate Member
+                    </h3>
+                    <h3 style={{marginLeft: '-110px', color: 'blue'}}>
+                        $
+                    </h3>
+                    <h1 style={{marginTop: '-30px', color: 'blue', fontSize: '80px'}}>
+                        20
+                    </h1>
+                    <h4 style={{padding: '20px'}}>
+                        Perfect for those with a professional interest in behavior analysis
+                    </h4>
+                    <h5>
+                        Valid for 12 months
+                    </h5>
+                </div>
+                
+                <div style={{width: '20%', backgroundColor: 'white', height: '700px'}}>
+                    <h3 style={{marginTop: '20px', marginBottom: '40px'}}>
+                        Membre régulier/Regular Member
+                    </h3>
+                    <h3 style={{marginLeft: '-110px', color: 'blue'}}>
+                        $
+                    </h3>
+                    <h1 style={{marginTop: '-30px', color: 'blue', fontSize: '80px'}}>
+                        30
+                    </h1>
+                    <h4 style={{padding: '20px'}}>
+                        Perfect for dedicated behavior analysts.
+                    </h4>
+                    <h5>
+                        Valid for 12 months
+                    </h5>
+                </div>
+                
+                <div style={{width: '20%', backgroundColor: 'white', height: '700px'}}>
+                    <h3 style={{marginTop: '20px', marginBottom: '40px'}}>
+                        Membre donateur/Sustaining Member
+                    </h3>
+                    <h3 style={{marginLeft: '-110px', color: 'blue'}}>
+                        $
+                    </h3>
+                    <h1 style={{marginTop: '-30px', color: 'blue', fontSize: '80px'}}>
+                        60
+                    </h1>
+                    <h4 style={{padding: '20px'}}>
+                        Perfect for champions of behavior analysis.
+                    </h4>
+                    <h5>
+                        Valid for 12 months
+                    </h5>
+                </div>
+            </div>
+
             <Footer />
         </div>
     )
